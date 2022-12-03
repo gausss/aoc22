7014fabd46d281db0f8e941ec07bcedca26a6a7f
gausss <gausssven@gmail.com>
2022-12-03 15:32:03 +0100

    Update structure

--- src/ex1/ex1.test.ts
+++ src/ex1/ex1.test.ts
@@ -1,24 +1,25 @@
 import {describe, expect, test} from '@jest/globals';
-import {solveEx1, solveEx1B, solveEx1Test, solveEx1TestB} from "./ex1";
+import {readInput, readTestInput} from "../utils";
+import {solveEx1A, solveEx1B} from "./ex1";
 
 describe('exercise 1', () => {
-    test('test part a', () => {
-        expect(solveEx1Test())
-            .toBe(24000);
+    test('test A', () => {
+        const elves = readTestInput("/ex1").split(/\r?\n\r?\n/);
+        expect(solveEx1A(elves)).toBe(24000);
     });
 
-    test('solve part a', () => {
-        expect(solveEx1())
-            .toBe(72240);
+    test('solve B', () => {
+        const elves = readInput("/ex1").split(/\r?\n\r?\n/);
+        expect(solveEx1A(elves)).toBe(72240);
     });
 
-    test('test part b', () => {
-        expect(solveEx1TestB())
-            .toBe(45000);
+    test('test B', () => {
+        const elves = readTestInput("/ex1").split(/\r?\n\r?\n/);
+        expect(solveEx1B(elves)).toBe(45000);
     });
 
-    test('solve part b', () => {
-        expect(solveEx1B())
-            .toBe(210957);
+    test('solve B', () => {
+        const elves = readInput("/ex1").split(/\r?\n\r?\n/);
+        expect(solveEx1B(elves)).toBe(210957);
     });
 });
--- src/ex1/ex1.ts
+++ src/ex1/ex1.ts
@@ -1,32 +1,13 @@
-import {readInput, readTestInput} from "../AocUtils";
-
-export function solveEx1(): number {
-    const elves = readInput("/ex1")
-        .split(/\r?\n\r?\n/);
+export function solveEx1A(elves: string[]): number {
     return findMostCalories(elves)[0];
 }
 
-export function solveEx1Test(): number {
-    const elves = readTestInput("/ex1")
-        .split(/\r?\n\r?\n/);
-    return findMostCalories(elves)[0];
-}
-
-export function solveEx1B(): number {
-    const elves = readInput("/ex1")
-        .split(/\r?\n\r?\n/);
-    return findMostCalories(elves)[0] + findMostCalories(elves)[1] + findMostCalories(elves)[2];
-}
-
-export function solveEx1TestB(): number {
-    const elves = readTestInput("/ex1")
-        .split(/\r?\n\r?\n/);
+export function solveEx1B(elves: string[]): number {
     return findMostCalories(elves)[0] + findMostCalories(elves)[1] + findMostCalories(elves)[2];
 }
 
 function findMostCalories(elves: string[]): number[] {
-    return elves.map((elf) => elf.split(/\r?\n/)
-                                 .map((calories) => +calories)
+    return elves.map((elf) => elf.split(/\r?\n/).map((calories) => +calories)
                                  .reduce((a, b) => a + b))
                 .sort((a, b) => b - a);
 }
